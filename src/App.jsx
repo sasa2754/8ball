1ef746c7728c0716941b4c0f790aa197fd6272bb
sasa2754 <sasamor5@gmail.com>
2024-09-17 19:49:10 -0300

    mt fofo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import style from './App.module.css'
-import respostas from '../palavras.json'
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -9,17 +8,37 @@ function getRandomInt(min, max) {
 }
 
 function App() {
-  const [resposta, setResposta] = useState('');
+  const [resposta, setResposta] = useState(undefined);
+  const [listReposta, setListaResposta] = useState([])
   
   useEffect(() => {
-    fetch('../palavras.json')
+    fetch('./palavras.json')
     .then((response) => response.json())
     .then((data) => {
-      console.log(response.data)
-      setResposta(data)
+      setListaResposta(data.palavras)
     })
-  })
+  }, [])
   
+  useEffect(() => {
+    if (!resposta)
+      return
+
+    const timeout = setTimeout(() => {
+      setResposta(undefined)
+    }, 2000)
+
+    return () => clearInterval(timeout)
+  }, [resposta])
+
+  const handleBallClick = () => {
+    if (listReposta.length == 0)
+      return
+
+    const p = listReposta[getRandomInt(0, listReposta.length)]
+
+    setResposta(p)
+  }
+
   return (
     <>
       <header>
@@ -27,10 +46,10 @@ function App() {
       </header>
 
       <main>
-        <img className={style.bola} src="./bola-8.png" alt="bola" onClick={() => {}}/>
-        <div>
-          <h1>Resposta: {resposta}</h1>
-        </div>
+        <img className={style.bola} src="./bola-8.png" alt="bola" onClick={handleBallClick}/>
+          <div className={style.resposta} style={{opacity: resposta ? "100%" : 0}}>
+            <h1>{resposta}</h1>
+          </div>
       </main>
     </>
   )
